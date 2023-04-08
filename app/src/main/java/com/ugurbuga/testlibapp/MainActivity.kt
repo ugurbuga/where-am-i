91af5b1ba8a3ccfa09617e45bee03913f5215944
Uğur Buğa <47065142+UgurBuga@users.noreply.github.com>
2023-04-08 16:52:42 +0300

    update
    update

--- app/src/main/java/com/ugurbuga/testlibapp/MainActivity.kt
+++ app/src/main/java/com/ugurbuga/testlibapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ugurbuga.testlibapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ugurbuga.testlib.TestFile
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TestFile.test()
     }
 }
