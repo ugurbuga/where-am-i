65fe0396c9e1801dc08406cf50b304309a5ed23a
Uğur Buğa <47065142+ugurbuga@users.noreply.github.com>
2023-04-11 23:58:45 +0300

    update log methods
    update log methods

--- WhereAmI/src/main/kotlin/com/ugurbuga/whereami/Logger.kt
+++ WhereAmI/src/main/kotlin/com/ugurbuga/whereami/Logger.kt
@@ -7,26 +7,20 @@ import androidx.fragment.app.Fragment
 
 internal object Logger {
 
-    private var lastActivity: String = ""
-    private var lastFragment: String = ""
     private const val WHERE_AM_I = "WhereAmI"
 
     fun logActivity(activity: Activity, logEnabled: Boolean) {
-        if (activity.toString() != lastActivity) {
-            lastActivity = activity.toString()
-            val activityName = activity.formattedName()
-            if (logEnabled) {
-                Log.d(WHERE_AM_I, activityName)
-            }
+        val activityName = activity.formattedName()
+        if (logEnabled) {
+            Log.d(WHERE_AM_I, activityName)
         }
     }
 
     fun logFragment(activity: Activity, fragment: Fragment, logEnabled: Boolean) {
-        if (fragment.toString() != lastFragment &&
-            fragment.tag !in ignoreList &&
-            !fragment.toString().startsWith(NAV_HOST_FRAGMENT)
+        if (fragment.tag !in ignoreList &&
+            !(fragment.toString().startsWith(NAV_HOST_FRAGMENT) ||
+                    fragment.toString().startsWith(SUPPORT_MAP_FRAGMENT))
         ) {
-            lastFragment = fragment.toString()
             val activityFragmentName =
                 activity.name().plus(" - ").plus(fragment.formattedName())
             if (logEnabled) {
@@ -40,8 +34,7 @@ internal object Logger {
     private const val GLIDE_MANAGER = "com.bumptech.glide.manager"
     private const val SUPPORT_LIFECYCLE_FRAGMENT_IMPL = "SupportLifecycleFragmentImpl"
 
-    private val ignoreList =
-        arrayListOf(GLIDE_MANAGER, SUPPORT_LIFECYCLE_FRAGMENT_IMPL, SUPPORT_MAP_FRAGMENT)
+    private val ignoreList = arrayListOf(GLIDE_MANAGER, SUPPORT_LIFECYCLE_FRAGMENT_IMPL)
 
     private fun Activity.name(): String = this.javaClass.simpleName
     private fun Activity.formattedName(): String {
@@ -68,5 +61,4 @@ internal object Logger {
             name()
         }
     }
-
 }
