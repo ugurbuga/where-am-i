e1fbf11e28a78662393729ab6c4c73dea8f1a2cc
Uğur Buğa <47065142+ugurbuga@users.noreply.github.com>
2023-06-20 16:00:54 +0300

    update file location

---
+++ WhereAmI/src/main/java/com/ugurbuga/whereami/WhereAmI.kt
@@ -0,0 +1,67 @@
+package com.ugurbuga.whereami
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+
+object WhereAmI {
+    fun init(application: Application, logEnabled: Boolean) {
+
+        var fragmentCallback: FragmentManager.FragmentLifecycleCallbacks? = null
+
+        val activityCallback = object : Application.ActivityLifecycleCallbacks {
+
+            override fun onActivityResumed(activity: Activity) {
+                Logger.logActivity(activity, logEnabled)
+                fragmentCallback = registerFragmentLifecycleCallbacks(activity, logEnabled)
+            }
+
+            override fun onActivityPaused(activity: Activity) {
+                unregisterFragmentLifecycleCallbacks(activity, fragmentCallback)
+                fragmentCallback = null
+            }
+
+            override fun onActivityStarted(activity: Activity) {}
+
+            override fun onActivityDestroyed(activity: Activity) {}
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+            override fun onActivityStopped(activity: Activity) {}
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+        }
+
+        application.registerActivityLifecycleCallbacks(activityCallback)
+    }
+
+    private fun registerFragmentLifecycleCallbacks(
+        activity: Activity,
+        logEnabled: Boolean
+    ): FragmentManager.FragmentLifecycleCallbacks? {
+        if (activity !is FragmentActivity) return null
+
+        val fragmentCallback = object : FragmentManager.FragmentLifecycleCallbacks() {
+            override fun onFragmentResumed(fm: FragmentManager, fragment: Fragment) {
+                super.onFragmentResumed(fm, fragment)
+                Logger.logFragment(activity, fragment, logEnabled)
+            }
+        }
+
+        activity.supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentCallback, true)
+
+        return fragmentCallback
+    }
+
+    private fun unregisterFragmentLifecycleCallbacks(
+        activity: Activity,
+        fragmentCallback: FragmentManager.FragmentLifecycleCallbacks?
+    ) {
+        if (activity !is FragmentActivity || fragmentCallback == null) return
+
+        activity.supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentCallback)
+    }
+}
