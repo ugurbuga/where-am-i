ec1a096fa0ab934102a8d5e566e6b27c61efe66d
Uğur Buğa <47065142+ugurbuga@users.noreply.github.com>
2023-06-22 14:25:27 +0300

    add example photo
    add example photo

--- app/src/main/java/com/ugurbuga/whereamiapp/App.kt
+++ app/src/main/java/com/ugurbuga/whereamiapp/App.kt
@@ -7,6 +7,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        WhereAmI.init(this, true)
+        WhereAmI.init(application = this, logEnabled = true)
     }
 }
