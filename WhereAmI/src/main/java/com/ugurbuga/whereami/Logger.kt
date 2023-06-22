47675349d398c8405f720b039a9ff6f4f49207b1
Uğur Buğa <47065142+ugurbuga@users.noreply.github.com>
2023-06-22 11:45:23 +0300

    update file name
    update file name

--- WhereAmI/src/main/java/com/ugurbuga/whereami/Logger.kt
+++ WhereAmI/src/main/java/com/ugurbuga/whereami/Logger.kt
@@ -1,32 +1,28 @@
 package com.ugurbuga.whereami
 
 import android.app.Activity
+import android.os.Build
 import android.util.Log
 import androidx.fragment.app.Fragment
 
 internal object Logger {
 
-    private var lastActivity: String = ""
-    private var lastFragment: String = ""
     private const val WHERE_AM_I = "WhereAmI"
 
     fun logActivity(activity: Activity, logEnabled: Boolean) {
-        if (activity.toString() != lastActivity) {
-            lastActivity = activity.toString()
-            val activityName = activity.name()
-            if (logEnabled) {
-                Log.d(WHERE_AM_I, activityName)
-            }
+        val activityName = getFormattedName(activity.javaClass)
+        if (logEnabled) {
+            Log.d(WHERE_AM_I, activityName)
         }
     }
 
     fun logFragment(activity: Activity, fragment: Fragment, logEnabled: Boolean) {
-        if (fragment.toString() != lastFragment &&
-            fragment.tag !in ignoreList &&
-            !fragment.toString().startsWith(NAV_HOST_FRAGMENT)
+        if (fragment.tag !in ignoreList &&
+            !(fragment.toString().startsWith(NAV_HOST_FRAGMENT) ||
+                    fragment.toString().startsWith(SUPPORT_MAP_FRAGMENT))
         ) {
-            lastFragment = fragment.toString()
-            val activityFragmentName = activity.name().plus(" - ").plus(fragment.name())
+            val activityFragmentName =
+                activity.javaClass.simpleName.plus(" - ").plus(getFormattedName(fragment.javaClass))
             if (logEnabled) {
                 Log.d(WHERE_AM_I, activityFragmentName)
             }
@@ -34,13 +30,21 @@ internal object Logger {
     }
 
     private const val NAV_HOST_FRAGMENT = "NavHostFragment"
+    private const val SUPPORT_MAP_FRAGMENT = "SupportMapFragment"
     private const val GLIDE_MANAGER = "com.bumptech.glide.manager"
     private const val SUPPORT_LIFECYCLE_FRAGMENT_IMPL = "SupportLifecycleFragmentImpl"
 
     private val ignoreList = arrayListOf(GLIDE_MANAGER, SUPPORT_LIFECYCLE_FRAGMENT_IMPL)
 
-    private fun Activity.name(): String = this.javaClass.simpleName
-
-    private fun Fragment.name(): String = this.javaClass.simpleName
-
+    private fun getFormattedName(clazz: Class<Any>): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            if (clazz.getDeclaredAnnotation(Metadata::class.java) != null) {
+                "(" + clazz.simpleName + ".kt:1)"
+            } else {
+                "(" + clazz.simpleName + ".java:1)"
+            }
+        } else {
+            clazz.simpleName
+        }
+    }
 }
